1cb25404386296c030e1a8bfe04b0966dafd08ce
bc <bara.catalin@gmail.com>
2021-05-21 10:19:02 +0300

    fix migrations

--- migrations/1621419985377-NftCollection.ts
+++ migrations/1621419985377-NftCollection.ts
@@ -4,18 +4,21 @@ export class NftCollection1621419985378 implements MigrationInterface {
   name = 'NftCollection1621419985378'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`create type nft_collection_source_enum as enum ('universe', 'scraper');`);
+    await queryRunner.query(`alter type nft_collection_source_enum owner to postgresusername;`);
+
     await queryRunner.query(`CREATE TABLE "nft_collection" (
-      "id" SERIAL NOT NULL, 
-      "userId" integer NOT NULL, 
-      "source" "nft_collection_source_enum" NOT NULL DEFAULT 'universe', 
-      "txHash" character varying, 
-      "onChain" boolean NOT NULL DEFAULT false, 
-      "address" character varying, 
-      "name" character varying NOT NULL, 
-      "symbol" character varying, 
-      "description" character varying, 
-      "createdAt" TIMESTAMP NOT NULL DEFAULT now(), 
-      "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), 
+      "id" SERIAL NOT NULL,
+      "userId" integer NOT NULL,
+      "source" "nft_collection_source_enum" NOT NULL DEFAULT 'universe',
+      "txHash" character varying,
+      "onChain" boolean NOT NULL DEFAULT false,
+      "address" character varying,
+      "name" character varying NOT NULL,
+      "symbol" character varying,
+      "description" character varying,
+      "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+      "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
       CONSTRAINT "PK_ffe58aa05707db77c2f20ecdbc3" PRIMARY KEY ("id")
     )`);
   }
--- migrations/1621419985378-Nft.ts
+++ migrations/1621419985378-Nft.ts
@@ -4,6 +4,10 @@ export class Nft1621419985378 implements MigrationInterface {
     name = 'Nft1621419985378'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`create type nft_source_enum as enum ('universe', 'scraper');`)
+
+        await queryRunner.query(`alter type nft_source_enum owner to postgresusername;`);
+
         await queryRunner.query(`CREATE TABLE "nft" ("id" SERIAL NOT NULL, "userId" integer NOT NULL, "source" "nft_source_enum" NOT NULL DEFAULT 'universe', "txHash" character varying, "onChain" boolean NOT NULL DEFAULT false, "name" character varying, "tokenId" character varying NOT NULL, "description" character varying, "artwork_type" character varying, "url" character varying, "optimized_url" character varying, "thumbnail_url" character varying, "original_url" character varying, "token_uri" character varying, "properties" jsonb, "royalties" real, "refreshed" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "collectionId" integer, CONSTRAINT "PK_8f46897c58e23b0e7bf6c8e56b0" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "nft" ADD CONSTRAINT "FK_941622072386aeece5112fe0db2" FOREIGN KEY ("collectionId") REFERENCES "nft_collection"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
