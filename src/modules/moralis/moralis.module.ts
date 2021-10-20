6ce19666ba01c2868c060cc11ffe0af01e5df076
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-10-20 03:56:13 +0300

    Add Moralis scraper

--- src/modules/moralis/moralis.module.ts
+++ src/modules/moralis/moralis.module.ts
@@ -1,7 +1,10 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppConfigModule } from '../configuration/configuration.module';
+import { FileStorageModule } from '../file-storage/file-storage.module';
+import { FileSystemModule } from '../file-system/file-system.module';
 import { NftCollection } from '../nft/domain/collection.entity';
+import { MonitoredNfts } from '../nft/domain/monitored-nfts';
 import { Nft } from '../nft/domain/nft.entity';
 import { QueueModel } from '../queue/queue.model';
 import { User } from '../users/user.entity';
@@ -9,7 +12,14 @@ import { MoralisService } from './moralis.service';
 import { MoralisWebHookController } from './moralis.webhook.controller';
 
 @Module({
-  imports: [AppConfigModule, QueueModel, HttpModule, TypeOrmModule.forFeature([Nft, NftCollection, User])],
+  imports: [
+    AppConfigModule,
+    QueueModel,
+    HttpModule,
+    TypeOrmModule.forFeature([Nft, NftCollection, User, MonitoredNfts]),
+    FileStorageModule,
+    FileSystemModule,
+  ],
   providers: [MoralisService],
   exports: [MoralisService],
   controllers: [MoralisWebHookController],
