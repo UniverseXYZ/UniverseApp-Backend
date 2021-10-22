5ba23601b6d25189d289b04eb74f173e8e9d518a
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-10-22 08:38:07 +0300

    Fix how network is choose by Moralis

--- src/modules/moralis/moralis.service.ts
+++ src/modules/moralis/moralis.service.ts
@@ -56,7 +56,7 @@ export class MoralisService {
   addNewUserToWatchAddress = async (address: string) => {
     Moralis.Cloud.run('watchEthAddress', {
       address,
-      chainId: process.env.NODE_ENV === 'development' ? '0x4' : '0x1',
+      chainId: this.config.values.ethereum.ethereumNetwork === 'rinkeby' ? '0x4' : '0x1',
       sync_historical: true,
     });
   };
