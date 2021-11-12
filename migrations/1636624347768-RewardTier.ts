62d477382179b07c422f4d0170e3d0d8a6fe7ee3
taskudis <85156108+taskudis@users.noreply.github.com>
2021-11-12 15:47:12 +0200

    fix: PR comments
    - Update exception statuses
    - Update migration steps

--- migrations/1636624347768-RewardTier.ts
+++ migrations/1636624347768-RewardTier.ts
@@ -4,15 +4,13 @@ export class RewardTier1636624347768 implements MigrationInterface {
     name = 'RewardTier1636624347768'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" RENAME COLUMN "minimumBid" TO "slots"`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" DROP COLUMN "slots"`);
+        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" DROP COLUMN "minimumBid"`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "slots" jsonb`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" DROP COLUMN "slots"`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "slots" numeric`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" RENAME COLUMN "slots" TO "minimumBid"`);
+        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "minimumBid" numeric`);
     }
 
 }
