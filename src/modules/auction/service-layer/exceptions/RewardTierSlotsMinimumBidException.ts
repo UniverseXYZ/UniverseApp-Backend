7a1d7c03d0298ddb5fbaa86ceb5f610ff9659ca0
taskudis <85156108+taskudis@users.noreply.github.com>
2021-11-15 14:17:16 +0200

    fix: PR comments
    - Update exception statuses
    - Update migration steps

--- src/modules/auction/service-layer/exceptions/RewardTierSlotsMinimumBidException.ts
+++ src/modules/auction/service-layer/exceptions/RewardTierSlotsMinimumBidException.ts
@@ -4,7 +4,7 @@ export class RewardTierSlotsMinimumBidException extends HttpException {
   constructor() {
     super(
       { error: 'RewardTierSlotsMinimumBidException', message: 'Reward Tiers slots minimumBid field error !' },
-      HttpStatus.NOT_FOUND,
+      HttpStatus.CONFLICT,
     );
   }
 }
