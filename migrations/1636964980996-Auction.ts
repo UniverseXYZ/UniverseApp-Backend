387a4bec25b2865e93eedde670f72018c2781bfa
vikinatora <vikinatora@gmail.com>
2021-11-17 14:06:08 +0200

    fix migration file

--- migrations/1636964980996-Auction.ts
+++ migrations/1636964980996-Auction.ts
@@ -5,7 +5,6 @@ export class Auction1636964980996 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction" DROP COLUMN "startingBid"`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."auction" DROP COLUMN "claimedFunds"`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."nft_collection" ALTER COLUMN "source" SET DEFAULT 'universe'`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."nft" ALTER COLUMN "source" SET DEFAULT 'universe'`);
     }
@@ -13,7 +12,6 @@ export class Auction1636964980996 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "universe-backend"."nft" ALTER COLUMN "source" SET DEFAULT 'universe'-backend".nft_source_enum`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."nft_collection" ALTER COLUMN "source" SET DEFAULT 'universe'-backend".nft_collection_source_enum`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."auction" ADD "claimedFunds" bigint`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction" ADD "startingBid" numeric NOT NULL`);
     }
 
