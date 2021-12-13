60148dba4dc55aa2170586dc1a96680854cb3235
vikinatora <vikinatora@gmail.com>
2021-12-13 10:08:23 +0200

    fix yet again

--- migrations/1637050723586-RewardTier.ts
+++ migrations/1637050723586-RewardTier.ts
@@ -5,12 +5,12 @@ export class RewardTier1637050723586 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" DROP COLUMN "nftsPerWinner"`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "nftsPerWinner" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "nftsPerWinner" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" DROP COLUMN "nftsPerWinner"`);
-        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "nftsPerWinner" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "universe-backend"."reward_tier" ADD "nftsPerWinner" integer`);
     }
 
 }
