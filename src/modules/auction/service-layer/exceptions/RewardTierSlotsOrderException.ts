bd326ffcb5c3550b857f6c9ef248cd2762a1e7f0
taskudis <85156108+taskudis@users.noreply.github.com>
2021-12-13 10:05:34 +0200

    fix: PR comments
    - Update exception statuses
    - Update migration steps

--- src/modules/auction/service-layer/exceptions/RewardTierSlotsOrderException.ts
+++ src/modules/auction/service-layer/exceptions/RewardTierSlotsOrderException.ts
@@ -2,9 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class RewardTierSlotsOrderException extends HttpException {
   constructor() {
-    super(
-      { error: 'RewardTierSlotsOrderException', message: 'Reward Tiers slots order error !' },
-      HttpStatus.NOT_FOUND,
-    );
+    super({ error: 'RewardTierSlotsOrderException', message: 'Reward Tiers slots order error !' }, HttpStatus.CONFLICT);
   }
 }
