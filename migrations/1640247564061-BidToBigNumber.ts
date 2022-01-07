82f87de3d15075812d4f8fb8dfdfc03114b6462e
vikinatora <vikinatora@gmail.com>
2022-01-07 09:08:04 +0200

    improve(migrations): merge migrations into one

--- migrations/1640247564061-BidToBigNumber.ts
+++ migrations/1640247564061-BidToBigNumber.ts
@@ -8,9 +8,11 @@ export class BidToBigNumber1640247564061 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" ADD "amount" bigint`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction" DROP COLUMN "revenueClaimed"`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction" ADD "revenueClaimed" bigint`);
-    }
+        await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" ADD "decimalPlaces" integer`);
+      }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" DROP COLUMN "decimalPlaces"`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction" DROP COLUMN "revenueClaimed"`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction" ADD "revenueClaimed" numeric(10,10) DEFAULT '0'`);
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" DROP COLUMN "amount"`);
