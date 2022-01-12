baf7e565a7e7b0c542d37c1d3d318379dcfc8952
vikinatora <vikinatora@gmail.com>
2022-01-12 13:14:13 +0200

    squash migrations into one file

--- migrations/1641916489551-AuctionBidWithdraw.ts
+++ migrations/1641916489551-AuctionBidWithdraw.ts
@@ -5,10 +5,14 @@ export class AuctionBidWithdraw1641916489551 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" ADD "withdrawn" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "universe-backend"."auction" ALTER COLUMN "revenueClaimed" SET DEFAULT '0'`);
+
      }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" DROP COLUMN "withdrawn"`);
+      await queryRunner.query(`ALTER TABLE "universe-backend"."auction" ALTER COLUMN "revenueClaimed" DROP DEFAULT`);
+      await queryRunner.query(`ALTER TABLE "universe-backend"."auction_bid" DROP COLUMN "withdrawn"`);
+
     }
 
 }
